5be76022d10c6dc5a5ba5cdf00087028b9e3e8c4
leandro.alves14 <=>
2023-04-01 10:46:37 -0300

    feat: add the counting of days left to the holliday

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import datetime
+from datetime import date
 
 def index(request):
 	return render(request, 'home.html')
 
+def calculate_days(f_day: int, f_month: int) -> int:
+	today = datetime.now()
+	f_date = date(today.year, f_month, f_day)
+	s_date = date(today.year, today.month, today.day)
+	return f_date - s_date
+
 def christmas(request):
 	time = datetime.now()
+	lasting = 0
 	christmas = True if time.month == 12 and time.day == 25 else False
-	return render(request, 'christmas.html', {'christmas': christmas})
+	if not christmas:
+		lasting = calculate_days(25, 12)
+	return render(request, 'christmas.html', {'christmas': christmas, 'lasting': lasting.days})
 
 def tiradentes(request):
 	time = datetime.now()
+	lasting = 0
 	tiradentes = True if time.month == 4 and time.day == 21 else False
-	return render(request, 'tiradentes.html', {'tiradentes': tiradentes})
+	if not tiradentes:
+		lasting = calculate_days(21, 4)
+	return render(request, 'tiradentes.html', {'tiradentes': tiradentes, 'lasting': lasting.days})
